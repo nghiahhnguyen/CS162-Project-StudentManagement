fd48ec913d66676a66a5e69c34a5b4ba1ee56520
Nghia Nguyen <34543734+huunghia160799@users.noreply.github.com>
2018-04-04 21:51:46 +0700

    Add function showMenu()

--- CS Project/Source.cpp
+++ CS Project/Source.cpp
@@ -1,12 +1,7 @@
 #include "Function.h"
 
 int main() {
-  classYear a;
-	char path[] = "17APCS1-Students.csv";
-	char path2[] = "Output.csv";
-
-	input(path, a);
-	output(path2, a);
+  
 	
 	
 	/*
@@ -22,13 +17,30 @@ int main() {
 	readUser("Test User.txt", user_pointer);
 	generateUsernameAndPassword(user_pointer);*/
 
-	a.head->changePassword();
-	cout << endl;
-	cout << a.head->password << endl;
+	classList class_list;
+	courseList course_list;
+	userList staff, lecturer;
+	class_list.head = NULL;
+	course_list.head = NULL;
+	staff.head = NULL;
+	lecturer.head = NULL;
 
-	classList L;
-	L.head = &a;
-	viewListOfClass(L);
+	//	create the first class in class list with the name first class
+	class_list.head=new classYear;
+	classYear *first_class = class_list.head;
+
+	char path[] = "17APCS1-Students.csv";
+	char path2[] = "Output.csv";
+	//	first student of the first class is initialized as NULL
+	//	to use the input function
+	first_class->head = NULL;
+	
+	input(path, *first_class);
+	output(path2, *first_class);
+
+	showMenu(class_list, course_list, staff, lecturer);
+
+
 
 	system("pause");
 	return 0;
